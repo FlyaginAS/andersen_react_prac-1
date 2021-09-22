aea7a3ebe7f1de14baa8538b936484ac18eea00e
FlyaginAS <rus.reg.36@gmail.com>
2021-09-22 13:51:34 +0300

    кнопка дизейблится и раздизейблится

--- cra/src/components/app/app.js
+++ cra/src/components/app/app.js
@@ -1,8 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './app.css';
 
-function App() {
-  return <div className="App">hello</div>;
-}
+export default class App extends Component {
+  state = {
+    value: '',
+  };
 
-export default App;
+  onInputChange = (evt) => this.setState({ value: evt.target.value });
+
+  checkInput = () => {
+    //учитывать или нет регистр сказано не было
+    return this.state.value.includes('реакт');
+  };
+
+  render() {
+    const {
+      state: { value },
+      onInputChange,
+      checkInput,
+    } = this;
+
+    return (
+      <form className="form">
+        <input
+          type="text"
+          className="input"
+          placeholder="Please, enter a text"
+          value={value}
+          onChange={onInputChange}
+        />
+        <button className="focus-button">Focus on input</button>
+        <button
+          type="submit"
+          className="submit-button"
+          disabled={checkInput()}
+        >
+          Submit
+        </button>
+      </form>
+    );
+  }
+}
